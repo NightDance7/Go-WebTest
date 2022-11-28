fb3c98fb2f0ca2f05c85a4014890baaefe32d21c
NightDance7 <mailforstudy1@mail.ru>
2022-11-28 09:47:31 +0300

    2test

--- main.go
+++ main.go
@@ -5,8 +5,26 @@ import (
 	"net/http"
 )
 
+type User struct {
+	name                  string
+	age                   uint16
+	money                 int16
+	avg_grades, happiness float64
+}
+
+func (u *User) getAllInfo() string {
+	return fmt.Sprintf("User name is: %s He is %d age old. He have %d "+
+		"money on count and %f level of happiness", u.name, u.age, u.money, u.happiness)
+
+}
+func (u *User) setNewName(newName string) {
+	u.name = newName
+}
+
 func home_page(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Go is super easy")
+	bob := User{"Bob", 25, -50, 4.2, 0.8}
+	bob.setNewName("Alex")
+	fmt.Fprintf(w, bob.getAllInfo())
 }
 
 func contacts_page(w http.ResponseWriter, r *http.Request) {
@@ -20,5 +38,6 @@ func handleRequest() {
 }
 
 func main() {
+
 	handleRequest()
 }
